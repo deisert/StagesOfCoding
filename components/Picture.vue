4c86a358d6e2c2a80975abc9ba03537f8ce09c94
Dominik <gamerdomi21@googlemail.com>
2021-12-24 16:00:49 +0100

    made picture blurrable on click events. first styling of picture

---
+++ components/Picture.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="flex justify-center">
+    <div class="picture-wrapper border-solid border-2 border-black mt-8">
+      <img
+        src="https://www.test.de/file/image/55/44/db4542ec-53d8-4a33-a876-331d463627e5-web/5779659_Passwortmanager_t202002.jpg"
+        alt=""
+        class="picture-test mb-6"
+        :class="{blur: isBlurred}"
+        @click="isBlurred = !isBlurred"
+      />
+
+    </div>
+  </div>
+
+
+</template>
+
+<script>
+export default {
+  name: "Pictures",
+
+  data: function () {
+    let isBlurred = true;
+
+    return {
+      isBlurred
+    }
+
+  },
+
+}
+</script>
+
+<style scoped>
+
+.picture-wrapper {
+  width: 200px;
+  height: 200px;
+}
+
+.picture-test {
+//width: 200px; //height:200px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <Tutorial/>
+  <div class="wrapper-container ma-0">
+    <div class="flex justify-center">
+      <h1 class="text-4xl mt-8">Coding with Daniel </h1>
+    </div>
+
+
+    <div>
+      <!--V-for every pic in pictures    -->
+      <Picture></Picture>
+    </div>
+
+  </div>
 </template>
 
 <script>
