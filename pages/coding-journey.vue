d469e0966b5d0d590900d7d853358e937ed5fb27
Dominik <gamerdomi21@googlemail.com>
2021-12-25 19:01:51 +0100

    Start Screen is now the index page. Added text on index.vue Added link to coding journey.

---
+++ pages/coding-journey.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wrapper-container ma-0">
+    <div class="flex justify-center">
+      <h1 class="text-4xl mt-8">Coding with Daniel </h1>
+    </div>
+
+    <div>
+      <!--V-for every pic in pictures    -->
+      <Picture></Picture>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "coding-journey"
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="wrapper-container ma-0">
-    <div class="flex justify-center">
-      <h1 class="text-4xl mt-8">Coding with Daniel </h1>
+  <div class="flex flex-col text-center wrapper my-8">
+    <h1 class="text-4xl">Start your journey</h1>
+
+    <div class="text-justify my-8 mx-4">
+      I had the pleasure to accompany you during your bachelor thesis.
+      You went through all the coding moods and I was privileged to watch.
+      Clever as I am, I made some screenshots of your mood.
+      Here are some of the said examples. <br>
+      Enjoy. <br> <br>
+      Happy Birthday btw. <br>
+      Love you. ❤ 😘
     </div>
 
-    <div>
-      <!--V-for every pic in pictures    -->
-      <Picture></Picture>
+    <div class="button-group flex flex-col justify-center">
+      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 mx-4 rounded my-3 px-4"
+         href="coding-journey"
+      >
+        Start
+      </a>
+
     </div>
-    <StartScreen></StartScreen>
+
   </div>
+
 </template>
 
 <script>
+//TODO: Add button component
 export default {
   name: 'IndexPage'
 }
