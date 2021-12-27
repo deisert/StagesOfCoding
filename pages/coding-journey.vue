fa7747e4bf2c4083417dae6085c89106f076a62d
Dominik <gamerdomi21@googlemail.com>
2021-12-28 00:01:29 +0100

    Additional text added / cleanup.

--- pages/coding-journey.vue
+++ pages/coding-journey.vue
@@ -1,25 +1,47 @@
 <template>
   <div class="wrapper-container ma-0">
     <div class="headline-container">
-      <h1 class="text-center text-4xl text-white-87 my-8">Coding Stages with Daniel </h1>
-      <h4 class="text-white-87 text-center mb-8">Click on the images to take a look</h4>
+      <h1 class="text-center text-4xl text-white-87 font-bold my-8">Coding Stages with Daniel </h1>
+      <h2 class="text-center text-3xl text-white-87 my-6">Basically <i>this</i> is a journey through your bachelor
+        thesis. </h2>
+      <h4 class="text-white-87 font-thin text-center mb-8">Click on the images to take a look</h4>
     </div>
 
     <div>
       <!--V-for every stage in stages -->
       <Stage stage-number="1"
-             bottom-text="debugging the console"/>
+             card-headline="Starting a new project"
+             card-text="Higly motivated. As usual when you start something new. "
+             img-src="Stage_1.png"/>
+
       <Stage stage-number="2"
-             bottom-text="Finding an Error"/>
+             img-src="Stage_2.png"
+             card-headline="Deep Work Face"
+             card-text="There are times when you are just one with your code. "/>
+
       <Stage stage-number="3"
-             bottom-text="Debugging the Error"/>
+             img-src="Stage_3.png"
+             card-headline="You realize you've chosen the wrong javascript framework. "
+             card-text="That face when you realize, Nuxt was kinda unneccessary. "/>
+
+      <Stage stage-number="4"
+             img-src="Stage_4.png"
+             card-headline="Writing your first tests. "
+             card-text="Questioning life choices.. "/>
+
+      <Stage stage-number="5"
+             img-src="Stage_5.png"
+             card-headline="3 Tests failed. "
+             card-text="Wait what? "/>
+
     </div>
   </div>
 </template>
 
 <script>
+
 export default {
-  name: "coding-journey"
+  name: "coding-journey",
 }
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,42 @@
 <template>
-  <div class="flex flex-col justify-center text-center container my-8 max-w-sm">
-    <h1 class="text-4xl text-white-87">Start your journey</h1>
+  <div class="flex flex-col items-center">
+    <div class="flex flex-col justify-center text-center container my-8">
+      <h1 class="text-4xl text-white-87">Start your journey</h1>
 
-    <div class="text-justify text-center text-white-87 my-8 mx-4">
-      I had the <i>pleasure</i> to accompany you during your bachelor thesis.
-      In which You went through all the different coding moods and I was <i>privileged</i> to watch <b>(live)</b>.
-      Clever as I am, I made some screenshots along the way.
-      Here are some examples. <br>
-      I hope you enjoy it as much as I do.<br> <br>
-      Happy Birthday btw. <br>
-      Love you. ❤ 😘
+      <div class="text-justify text-center text-white-87 my-8 mx-4">
+        I had the <i>pleasure</i> to accompany you during your bachelor thesis.
+        In which You went through all the different coding moods and I was <i>privileged</i> to watch <b>(live)</b>.
+        Clever as I am, I made some screenshots along the way.
+        Here are some examples. <br>
+        I hope you enjoy it as much as I do.<br> <br>
+        Happy Birthday btw. <br>
+        Love you. ❤️ 😘
+      </div>
+
+      <div class="button-group flex flex-col justify-center">
+        <a
+          class="bg-gradient-to-r from-cyan-800 to-indigo-900 text-white-87 shadow-md border border-indigo-900 font-bold py-2 mx-4 rounded my-3 px-4"
+          href="coding-journey"
+        >
+          Start
+        </a>
+      </div>
     </div>
-
-    <div class="button-group flex flex-col justify-center">
-      <a class="bg-indigo-600 hover:bg-indigo-800 text-white-87 font-bold py-2 mx-4 rounded my-3 px-4"
-         href="coding-journey"
-      >
-        Start
-      </a>
-
-    </div>
-
   </div>
 
 </template>
 
 <script>
-//TODO: Add button component
 export default {
   name: 'IndexPage'
 }
 </script>
+
+<style scoped>
+.container {
+  max-width: 28rem;
+  display: flex;
+  justify-content: center;
+}
+
+</style>
